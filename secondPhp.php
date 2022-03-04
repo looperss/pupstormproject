cb5a6f6ae0ccb1de8d589029d306010c3639f5d0
looperss <qq2324144106@163.com>
2022-03-04 23:50:27 +0800

    phpstorm1

--- secondPhp.php
+++ secondPhp.php
@@ -1,1 +1,37 @@
+<!DOCTYPE html>
+<html lang="en"></html>
+<head>
+    <meta charset="UTF-8">
+    <title>secondPage</title>
+    <link rel="stylesheet" href="css/secondPhpCss.css">
+</head>
+<body>
 <?php
+    echo "这是内嵌在secondPhp html的php"
+?>
+<div>
+    <div>
+        <P>原料信息展示</P>
+    </div>
+    <div>
+        <table>
+            <tr>
+                <td>invId</td>
+                <td>funcId</td>
+                <td>invCode</td>
+                <td>invName</td>
+                <td>invSpecies</td>
+                <td>invClassId</td>
+                <td>gwDiff</td>
+                <td>thicknessInMM</td>
+                <td>widthInMM</td>
+                <td>lengthInMM</td>
+                <td>remark</td>
+                <td>createPerson</td>
+                <td>createDate</td>
+            </tr>
+        </table>
+    </div>
+</div>
+</body>
+
